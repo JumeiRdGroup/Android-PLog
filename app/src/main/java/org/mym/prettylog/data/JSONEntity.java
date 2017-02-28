9e64abc2ff646bf6ff887183c0659b4e1266461b
Muyangmin <muyangmin@foxmail.com>
2017-02-28 18:50:31 +0800

    Update sample to show multiple json usage. [ci skip]

--- app/src/main/java/org/mym/prettylog/data/JSONEntity.java
+++ app/src/main/java/org/mym/prettylog/data/JSONEntity.java
@@ -9,4 +9,50 @@ public class JSONEntity {
     public static final String DATA = "{\"favSites\":[{\"name\":\"google\",\"domain\":\"https:www" +
             ".google.com\"},{\"name\":\"facebook\",\"domain\":\"https:www.facebook.com\"}]," +
             "\"user\":{\"name\":\"Lari\",\"address\":null,\"cert\":true,\"preserved\":{}}}";
+
+    public static final String DATA1 = "{\"res\":true,\"data\":{\"id\":\"3\"," +
+            "\"icon\":\"http:\\/\\/182.254.246" +
+            ".232:8080\\/group1\\/M00\\/00\\/2E\\/Cvc9DVhGfa2AB2L8AAFB3Qh8cK4333.jpg\"," +
+            "\"begin_time\":\"2016-02-18\",\"end_time\":\"2017-01-06\"}}";
+
+    public static final String DATA2 = "{\"res\":true,\"data\":[{\"id\":\"164\"," +
+            "\"image\":\"http:\\/\\/182.254.246" +
+            ".232:8080\\/group1\\/M00\\/00\\/4A\\/Cvc9DVizzdiAKY1jAAKRlAaLiKU118.png\"," +
+            "\"name\":\"\\u6d4b\\u8bd5\\u670d\\u52a1963\",\"description\":\"\",\"url\":\"\"," +
+            "\"android_url\":\"122\",\"ios_url\":\"123\",\"share_title\":\"\"," +
+            "\"share_desc\":\"\",\"share_img\":\"\",\"share_url\":\"\"},{\"id\":\"163\"," +
+            "\"image\":\"http:\\/\\/182.254.246" +
+            ".232:8080\\/group1\\/M00\\/00\\/2A\\/Cvc9DVefAlKAP7h5AAOOMQZ-h_Q891.jpg\"," +
+            "\"name\":\"\\u5317\\u4eac\\u9996\\u9875\\u5f39\\u51fa\\u5f0f\\u6d3b\\u52a8-\\u706b" +
+            "\\u4e0a\\u56fe\\u7247111\"," +
+            "\"description\":\"qwewe\\u63cf\\u8ff0\\u63cf\\u8ff0\\u63cf\\u8ff0\\u63cf\\u8ff0" +
+            "\\u63cf\\u8ff0123\",\"url\":\"\",\"android_url\":\"\"," +
+            "\"ios_url\":\"YouHuiQuanInfoViewController\"," +
+            "\"share_title\":\"1\\u5206\\u4eab\\u6807\\u9898 " +
+            "*\\u539a\\u5927\\u53f8\\u8003\\u79ef\\u5206\\u4f1a729387\"," +
+            "\"share_desc\":\"2\\u5206\\u4eab\\u63cf\\u8ff0 *@#\\uffe5@#8988809\"," +
+            "\"share_img\":\"http:\\/\\/182.254.246" +
+            ".232:8080\\/group1\\/M00\\/00\\/28\\/Cvc9DVeC_ueAaqzNAAJ39TRp3Kw284.jpg\"," +
+            "\"share_url\":\"https:\\/\\/www.hao123.com\"},{\"id\":\"161\"," +
+            "\"image\":\"http:\\/\\/182.254.246" +
+            ".232:8080\\/group1\\/M00\\/00\\/2E\\/Cvc9DVhHouWAGG8TAABqLfQC_gM131.jpg\"," +
+            "\"name\":\"\\u6d4b\\u8bd5\\u8bf7\\u6c42\",\"description\":\"\"," +
+            "\"url\":\"http:\\/\\/www.hao123.com\",\"android_url\":\"\",\"ios_url\":\"\"," +
+            "\"share_title\":\"123\",\"share_desc\":\"123\",\"share_img\":\"http:\\/\\/182.254" +
+            ".246.232:8080\\/group1\\/M00\\/00\\/4A\\/Cvc9DViqlEaAXZLBAAAlZ9C9s9E767.png\"," +
+            "\"share_url\":\"http:\\/\\/www.hao123.com\"},{\"id\":\"160\"," +
+            "\"image\":\"http:\\/\\/182.254.246" +
+            ".232:8080\\/group1\\/M00\\/00\\/2B\\/Cvc9DVe0PleAA70nAAA5_udnrHw255.png\"," +
+            "\"name\":\"\\u7838\\u91d1\\u86cb\",\"description\":\"\",\"url\":\"http:\\/\\/www" +
+            ".hao123.com\",\"android_url\":\"\",\"ios_url\":\"\",\"share_title\":\"\"," +
+            "\"share_desc\":\"\",\"share_img\":\"\",\"share_url\":\"\"},{\"id\":\"143\"," +
+            "\"image\":\"http:\\/\\/182.254.246" +
+            ".232:8080\\/group1\\/M00\\/00\\/2A\\/Cvc9DVekSpiAQT3rAANCQokEWDs795.png\"," +
+            "\"name\":\"\\u65e0\\u9521\\u6d3b\\u52a8\",\"description\":\"\",\"url\":\"\"," +
+            "\"android_url\":\"\",\"ios_url\":\"\",\"share_title\":\"\",\"share_desc\":\"\"," +
+            "\"share_img\":\"\",\"share_url\":\"\"},{\"id\":\"141\",\"image\":\"http:\\/\\/182" +
+            ".254.246.232:8080\\/group1\\/M00\\/00\\/18\\/Cvc9DVbFaAaAKDPSAAKXIjV3N8Y263.jpg\"," +
+            "\"name\":\"\\u4e0a\\u6d77\",\"description\":\"\",\"url\":\"\",\"android_url\":\"\"," +
+            "\"ios_url\":\"\",\"share_title\":\"\",\"share_desc\":\"\",\"share_img\":\"\"," +
+            "\"share_url\":\"\"}]}";
 }
